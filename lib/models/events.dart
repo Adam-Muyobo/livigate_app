398b0d296e516a50100666acc7186855f4d2cd25
Adam Muyobo <95537354+Adam-Muyobo@users.noreply.github.com>
2022-08-29 23:22:08 +0200

    Added flyer to event screen and get ticket button.

--- lib/models/events.dart
+++ lib/models/events.dart
@@ -4,13 +4,13 @@ class Events{
   String description;
   String eventImage;
   String organisers;
+  String ticketDetails;
 
 
   Events(this.eventName, this.location, this.description, this.eventImage,
-      this.organisers, this.dateTime);
+      this.organisers, this.dateTime, this.ticketDetails);
 
   String dateTime;
-
 
 
   static List<Events> getEvents(){
@@ -18,26 +18,76 @@ class Events{
     events.add(Events(
       'Jersey Mania',
       'Bangwe Bush house',
-      '',
-      'Dijo media',
+        'Dijo media',
+      'https://media.istockphoto.com/photos/friends-are-watching-the-soccer-match-and-drinking-beer-on-the-bar-picture-id903977120?b=1&k=20&m=903977120&s=170667a&w=0&h=k5IA1KZAVhWuzecDqOiqQgz0tbbDRnq3tXMigiV6RGc=',
       'Jersey party late at night each ticket comes with a jersey and a round of drinks',
-      '2022-08-20'
+      '2022-08-20',
+      ''
     ));
     events.add(Events(
         'Dash Colour Fest',
         'Jarring Park',
-        '',
-        'Dijo media',
+        'Kings media',
+        'https://images.unsplash.com/photo-1603228254119-e6a4d095dc59?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29sb3VyJTIwZmVzdGl2YWx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
         'Colour full festival full of people powdered colour and nice drinks and lots of food and child friendly entertainment',
-        '2022-08-30'
+        '2022-08-30',
+        ''
     ));
     events.add(Events(
         'Chumas Cook out',
         'Mukuni Park',
-        '',
         'Dijo media',
+        'https://media.istockphoto.com/photos/simple-round-charcoal-barbecue-gril-closeup-picture-id1390588637?b=1&k=20&m=1390588637&s=170667a&w=0&h=9BB4LNsfsXOz8NunfJBHSfuFYo-8UmAtVvy4Nd2EbSQ=',
         'Cooking never got more competitive with this one invite your friends and family with their best dishes and various cuisines',
-        '2022-08-30'
+        '2022-08-30',
+        ''
+    ));
+    events.add(Events(
+        'Smoke and Asphalt 11"',
+        'NASDEC',
+        'Zed Crank Shaft',
+        'https://media.istockphoto.com/photos/classic-street-rod-automobile-lineup-at-frog-follies-car-show-picture-id171224108?b=1&k=20&m=171224108&s=170667a&w=0&h=wUd-fsQROjVSYWfPMJY_hcJfxxu2GCAFxxWZ7GsxEyA=',
+        'We are back and better, come one come all to the 11th S and A motor show,'
+            ' if you have been around you already know whats happening if you\'re here for the first time,'
+            'well whether your a flair or performance person S and A has  all that for you get in gear and come cruise with us',
+        '2022-08-30',
+        ''
+    ));
+    events.add(Events(
+        'Freshers Ball',
+        'Mukuni Park',
+        'Dijo media',
+        '',
+        'Cooking never got more competitive with this one invite your friends and family with their best dishes and various cuisines',
+        '2022-08-30',
+        ''
+    ));
+    events.add(Events(
+        'Grammys',
+        'Mukuni Park',
+        'Dijo media',
+        'https://images.unsplash.com/photo-1518893883800-45cd0954574b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Z3JhbW15c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+        'Cooking never got more competitive with this one invite your friends and family with their best dishes and various cuisines',
+        '2022-08-30',
+        ''
+    ));
+    events.add(Events(
+        'Semester ShutDown',
+        'Mukuni Park',
+        'Dijo media',
+        '',
+        'Cooking never got more competitive with this one invite your friends and family with their best dishes and various cuisines',
+        '2022-08-30',
+        ''
+    ));
+    events.add(Events(
+        'The Big Bang Release',
+        'Mukuni Park',
+        'Dijo media',
+        'https://media.istockphoto.com/photos/catwalk-picture-id182210083?b=1&k=20&m=182210083&s=170667a&w=0&h=JuPKaD66aq9ssyxQU83e9I4aJBZM2a6dX0gP-i17ghY=',
+        'Cooking never got more competitive with this one invite your friends and family with their best dishes and various cuisines',
+        '2022-08-30',
+        ''
     ));
 
     return events;
--- lib/pages/model_screens/event_screen.dart
+++ lib/pages/model_screens/event_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:my_app/models/events.dart';
 
 class EventScreen extends StatelessWidget {
@@ -9,9 +11,77 @@ class EventScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.orangeAccent[100],
-      body: Container(
-        color: Colors.orangeAccent[100],
-        child: Center(child: Text(events.eventName)),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 4,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 3, top: 11),
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    image: NetworkImage(events.eventImage),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      bottom: 20,
+                      child: Container(
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: const Color.fromARGB(100, 22, 44, 33),
+                        ),
+                        child: Text(
+                          events.eventName,
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20, top: 10),
+              child: GestureDetector(
+                onTap: (){
+                  if (kDebugMode) {
+                    print('Book me for ${events.eventName}');
+                  }
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(15)
+                  ),
+                  child: Center(
+                      child: Text(
+                    'Get Ticket',
+                    style: GoogleFonts.raleway(
+                      color: Colors.orange[50],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
+                  )),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
